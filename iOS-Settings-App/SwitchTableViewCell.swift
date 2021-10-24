87a777b37a2293331e249232b138f98a92176027
Sergei <smyzin.work@gmail.com>
2021-10-24 23:37:19 +0300

    Init App
    First commit init

---
+++ iOS-Settings-App/SwitchTableViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  SettingTableViewCell.swift
+//  SettingsApp2
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    static let identifier = "SwitchTableViewCell"
+    
+    private let iconContainer: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 8
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private let iconImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let _switch: UISwitch = {
+        let _switch = UISwitch()
+        _switch.onTintColor = .systemGreen
+        return _switch
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(label)
+        contentView.addSubview(iconContainer)
+        contentView.addSubview(_switch)
+        iconContainer.addSubview(iconImageView)
+        
+        contentView.clipsToBounds = true
+        accessoryType = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size: CGFloat = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
+        
+        let imageSize: CGFloat = size/1.5
+        iconImageView.frame = CGRect(x: (size - imageSize)/2, y: (size - imageSize)/2, width: imageSize, height: imageSize)
+        
+        _switch.sizeToFit()
+        _switch.frame = CGRect(
+            x: contentView.frame.size.width - _switch.frame.size.width - 20,
+            y: (contentView.frame.size.height - _switch.frame.size.height)/2,
+            width: _switch.frame.size.width,
+            height: _switch.frame.size.height)
+        
+        label.frame = CGRect(
+            x: 25 + iconContainer.frame.size.width,
+            y: 0,
+            width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
+            height: contentView.frame.size.height)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.image = nil
+        label.text = nil
+        iconContainer.backgroundColor = nil
+        _switch.isOn = false
+    }
+    
+    public func configure(with model: SettingsSwitchOption) {
+        label.text = model.title
+        iconImageView.image = model.icon
+        iconContainer.backgroundColor = model.iconBackgroundColor
+        _switch.isOn = model.isOn
+    }
+}
